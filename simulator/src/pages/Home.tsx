defba1280eeba0cc5fff5c8146a4656c91bd5d26
saadih <120604473+saadih@users.noreply.github.com>
2025-06-09 15:29:53 +0200

    final fixes

--- simulator/src/pages/Home.tsx
+++ simulator/src/pages/Home.tsx
@@ -158,9 +158,7 @@ const Home: React.FC = () => {
               <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
             </div>
           )}
-          {/* Result table */}
           {showResult && !isLoading && (
-
             <Result data={dataRows} provider={provider} />
           )}
         </div>
